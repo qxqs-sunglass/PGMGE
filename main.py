c89139f7e7363b19c8e62644c9e1cb8f6449c09e
黄文浩 <18259936929@163.com>
2025-07-05 21:17:56 +0800

    day +1

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 """游戏引擎主程序"""
+
+
+class Main:
+    def __init__(self):
+        self.__author__ = '@bilibili__秋天会有下雪天'
+        self.__version__ = '0.0.1'
+        self.__date__ = '2025-07-5'
+
+
+if __name__ == '__main__':
+    pass
+
+
